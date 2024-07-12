17a7230f165d93e576c52aa8120d5dd57ef334b5
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-11 21:33:12 -0500

    Add function getURLsFromHTML

--- crawl.js
+++ crawl.js
@@ -1,6 +1,21 @@
+import { JSDOM } from 'jsdom';
+
 const normalizeURL = (url) => {
   const newURL = new URL(url);
   return `${newURL.hostname}${newURL.pathname}`.replace(/\/$/, '');
 };
 
-export { normalizeURL };
+const getURLsFromHTML = (htmlBody, baseURL) => {
+  const dom = new JSDOM(htmlBody);
+  const urls = [];
+  const anchors = dom.window.document.querySelectorAll('a');
+  for (const anchor of anchors) {
+    const url = anchor.getAttribute('href');
+
+    const newURL = new URL(url, baseURL).href;
+    urls.push(newURL);
+  }
+  return urls;
+};
+
+export { normalizeURL, getURLsFromHTML };
