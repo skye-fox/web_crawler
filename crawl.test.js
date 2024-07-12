a2d5a1a8ae7e79548a924b92f541e140fd9cfddf
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-11 21:33:42 -0500

    Add tests for getURLsFromHTML

--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@jest/globals';
-import { normalizeURL } from './crawl';
+import { normalizeURL, getURLsFromHTML } from './crawl';
 
 test('removes the http://', () => {
   expect(normalizeURL('http://boot.dev/path')).toBe('boot.dev/path');
@@ -19,4 +19,25 @@ test('removes the https:// & the trailing /', () => {
   expect(normalizeURL('https://boot.dev/path/to/stuff/')).toBe(
     'boot.dev/path/to/stuff',
   );
+});
+
+test('relative URL to absolute URL', () => {
+  expect(
+    getURLsFromHTML(
+      '<html><body><a href="/path/to/somewhere/">boot.dev</a></body></html>',
+      'https://blog.boot.dev',
+    ),
+  ).toEqual(['https://blog.boot.dev/path/to/somewhere/']);
+});
+
+test('relative URL to absolute URL & absolute URL', () => {
+  const inputURL = 'https://blog.boot.dev';
+  const inputBody =
+    '<html><body><a href="/path/to/somewhere/">boot.dev</a><a href="https://www.example.com/path/to/somewhere/">example.com</a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = [
+    'https://blog.boot.dev/path/to/somewhere/',
+    'https://www.example.com/path/to/somewhere/',
+  ];
+  expect(actual).toEqual(expected);
 });
